98af0fa2f97d303a230319c1cf31b3aab8e7e776
Heath Patterson <nheathpatterson@gmail.com>
2021-12-27 12:53:13 -0600

    Make all control groups collapsible for visibility

--- napari_imsmicrolink/qwidgets/ims_ctl_group.py
+++ napari_imsmicrolink/qwidgets/ims_ctl_group.py
@@ -1,12 +1,19 @@
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QGroupBox
+from qtpy.QtWidgets import QWidget, QGroupBox, QVBoxLayout, QSizePolicy
+from superqt import QCollapsible
 from napari_imsmicrolink.qwidgets.subwidgets.control_ims_info import CntrlIMS
 
 
 class IMSControl(QWidget):
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.ims_ctrl_frame = QGroupBox()
-        self.ims_ctrl_frame.setTitle("Imaging MS control")
-        self.ims_ctrl_frame.setLayout(QVBoxLayout())
+        self.ims_ctrl_frame = QCollapsible(title="Imaging MS padding")
+        self.ims_ctrl_frame.setSizePolicy(
+            QSizePolicy.Minimum, QSizePolicy.MinimumExpanding
+        )
+
+        self.ims_ctrl_box = QGroupBox()
+
+        self.ims_ctrl_box.setLayout(QVBoxLayout())
         self.ims_ctl = CntrlIMS()
-        self.ims_ctrl_frame.layout().addWidget(self.ims_ctl)
+        self.ims_ctrl_box.layout().addWidget(self.ims_ctl)
+        self.ims_ctrl_frame.addWidget(self.ims_ctrl_box)
--- napari_imsmicrolink/qwidgets/tform_ctl_group.py
+++ napari_imsmicrolink/qwidgets/tform_ctl_group.py
@@ -1,4 +1,5 @@
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QTabWidget
+from qtpy.QtWidgets import QWidget, QTabWidget, QSizePolicy
+from superqt import QCollapsible
 from napari_imsmicrolink.qwidgets.subwidgets.control_transform import CntrlTransform
 from napari_imsmicrolink.qwidgets.subwidgets.control_rotation import CntrlRotation
 
@@ -6,14 +7,16 @@ from napari_imsmicrolink.qwidgets.subwidgets.control_rotation import CntrlRotati
 class TformControl(QWidget):
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.tform_ctrl_frame = QGroupBox()
-        self.tform_ctrl_frame.setTitle("Transformation control")
-        self.tform_ctrl_frame.setLayout(QVBoxLayout())
+        self.tform_ctrl_frame = QCollapsible(title="Transformation control")
+        self.tform_ctrl_frame.setSizePolicy(
+            QSizePolicy.Minimum, QSizePolicy.MinimumExpanding
+        )
+
         self.tform_tabs = QTabWidget()
         self.tform_ctl = CntrlTransform()
         self.rot_ctl = CntrlRotation()
 
         self.tform_tabs.addTab(self.tform_ctl, "Transform")
         self.tform_tabs.addTab(self.rot_ctl, "Orientation")
-        self.tform_ctrl_frame.layout().addWidget(self.tform_tabs)
-        self.tform_ctrl_frame.layout().addStretch(1)
+        self.tform_ctrl_frame.addWidget(self.tform_tabs)
+        self.tform_ctrl_frame.expand(animate=False)
